e0a55b927612f5f5980f9d6524003e9be316bb12
Celine <68299431+celinegiang@users.noreply.github.com>
2026-09-22 16:36:27 +0000

    editing

--- lab: server and client/client.py
+++ lab: server and client/client.py
@@ -9,3 +9,7 @@ msg = "hi"
 
 # send the message, using the socket
 ### send(__msg___, __destination__)
+dest = ('127.0.0.1', 12345)
+# encode the message into bytes
+msg = msg.encode()
+clientSocket.sendto(msg, dest)
