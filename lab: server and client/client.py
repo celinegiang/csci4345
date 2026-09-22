fce7a6237f5aca3e90cd6165a688e2dab5b362a9
Celine <68299431+celinegiang@users.noreply.github.com>
2026-09-22 17:05:47 +0000

    added continuous input and loop

--- lab: server and client/client.py
+++ lab: server and client/client.py
@@ -4,12 +4,16 @@ from socket import *
 # create a socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-# create a message
-msg = "hi"
+while True:
+    # create a message
+    msg = input()
 
-# send the message, using the socket
-### send(__msg___, __destination__)
-dest = ('127.0.0.1', 12345)
-# encode the message into bytes
-msg = msg.encode()
-clientSocket.sendto(msg, dest)
+    # send the message, using the socket
+    ### send(__msg___, __destination__)
+    dest = ('127.0.0.1', 12345)
+    # encode the message into bytes
+    msg = msg.encode()
+    clientSocket.sendto(msg, dest)
+
+    if msg.decode() == 'exit':
+        break
--- lab: server and client/server.py
+++ lab: server and client/server.py
@@ -6,8 +6,12 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 # make this socket bound to a port 
 serverSocket.bind(('127.0.0.1', 12345))
 
-# receive a message
-msg = serverSocket.recvfrom(2048)
+while True:
+    # receive a message
+    msg = serverSocket.recvfrom(2048)
 
-# print the message
-print(msg)
+    # print the message
+    print(msg)
+
+    if msg == 'exit':
+        break
